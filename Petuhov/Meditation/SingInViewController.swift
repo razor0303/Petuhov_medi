df784ffec780dca4aae391f573405988f73aa3f5
razor0303 <81754411+razor0303@users.noreply.github.com>
2022-02-28 12:05:22 +0300

    Add files via upload

---
+++ Petuhov/Meditation/SingInViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  Meditation
+//
+//  Created by Student on 30.11.2021.
+//
+
+import UIKit
+import Alamofire
+import SwiftyJSON
+
+class SingInViewController: UIViewController {
+
+    @IBOutlet weak var inputPassword: UITextField!
+    @IBAction func signInAction(_ sender: UIButton) {
+    }
+}
+
